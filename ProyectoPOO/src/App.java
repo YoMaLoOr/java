dd3667a19e121bf282b4f696f1a7510c6c3b71cb
YoMaLoOr <yoemanuel1998@gmail.com>
2025-06-16 13:42:46 +0200

    16-06-2025

--- ProyectoPOO/src/App.java
+++ ProyectoPOO/src/App.java
@@ -2,11 +2,29 @@ import com.objetos.Persona;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Probando Persona.");
-        Persona guy = new Persona();
-        guy.name = "Pedro";
-        guy.surname = "Jimenez";
-        guy.age = 25;
-        System.out.println(guy.name + " " + guy.surname + ", edad: " + guy.age);
+        System.out.println("Probando la clase Persona");
+        try {
+            //CUANDO HABLAMOS DE CLASES, NECESITAMOS OBJETOS PARA 
+            //TRABAJAR CON DICHAS CLASES
+            //INSTANCIAMOS EL OBJETO PERSONA
+            Persona personaje = new Persona();
+            //YA PODEMOS UTILIZAR LA PERSONA Y SUS PROPIEDADES
+            personaje.setNombre("Jon");
+            personaje.setApellidos("Nieve");
+            personaje.setEdad(29);
+            System.out.println(personaje.getNombre() + " "
+            + personaje.getApellidos() + ", Edad: " + personaje.getEdad()); 
+            personaje.setGenero(Persona.tipoGenero.MASCULINO);
+            System.out.println("Género: " + personaje.getGenero());
+            System.out.println(personaje.getNombreCompleto());
+            System.out.println(personaje.getNombreCompleto(true));
+            System.out.println(personaje.getNombreCompleto(19));
+            System.out.println("Dni del personaje: ");
+            personaje.setDni(12345678);
+            System.out.println("Letra del dni: " + personaje.getLetrasnif());
+        } catch (Exception e) {
+            //NUNCA DEJAREMOS UN CATCH VACIO
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
--- ProyectoPOO/src/com/objetos/Persona.java
+++ ProyectoPOO/src/com/objetos/Persona.java
@@ -1,8 +1,101 @@
 package com.objetos;
 
 public class Persona {
-    public String name;
-    public String surname;
-    public int age;
+    //debemos declarar las variables privadas de 
+    //cada propiedad
+    private String nombre;
+    private String apellidos;
+    private int edad;
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return this.apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public void setEdad(int edad) throws Exception
+    {
+        //debemos controlar la edad y realizar la acción 
+        //que más nos guste
+        //2) error grave: El programador sabe que 
+        //esto le puede dar un error, que lo controle.
+        if (edad < 0){
+            throw new Exception("La edad no puede ser negativa");
+        }else {
+            this.edad = edad;
+        }
+    }
+
+    public enum tipoGenero {
+        FEMENINO, MASCULINO, OTROS
+    }
+
+    private tipoGenero genero;
+
+    public tipoGenero getGenero() {
+        return this.genero;
+    }
+
+    public void setGenero(tipoGenero genero) {
+        this.genero = genero;
+    }
+
+    //METODO PARA DEVOLVER EL NOMBRE Y APELLIDOS
+    public String getNombreCompleto(){
+        return this.nombre + " " + this.apellidos;
+    }
+
+    //METODO PARA DEVOLVER EL NOMBRE Y APELLIDOS EN MAYUSCULAS
+    public String getNombreCompleto(int numero){
+        return this.nombre.toUpperCase() 
+        + " " + this.apellidos.toUpperCase();
+    }
     
+    //METODO PARA DEVOLVER APELLIDOS Y NOMBRE
+    public String getNombreCompleto(boolean orden){
+        return this.apellidos + " " + this.nombre;
+    }
+
+    //METODO PARA DEVOLVER NOMBRE Y APELLIDOS MEDIANTE
+    //EL SEPARADOR QUE DECIDA EL PROGRAMADOR
+    public String getNombreCompleto(String separador){
+        return this.nombre + separador + this.apellidos;
+    }
+
+    //LO QUE IMPORTA ES LA FIRMA DE UN METODO
+    public void getNombreCompleto(int num1, int num2){}
+
+    public int getNombreCompleto(String a, int b){
+        return 19;
+    }
+    private int dni;
+
+    public int getDni() {
+        return this.dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public char getLetrasnif(){
+        String dniChars = "TRWAGMYFPDXBNJZSQVHLCKET";
+        int resul = (dni - (dni / 23) * 23);
+        char type = dniChars.charAt(resul);
+        return type;
+    }
 }
